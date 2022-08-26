2315fe9b4d64c880b2728cb73e7b1a2a33bb5e19
Marcin Bednarz <mbz@amazon.com>
2022-08-26 11:45:27 -0500

    updated GG

--- install-gg.sh
+++ install-gg.sh
@@ -5,5 +5,5 @@ sudo apt install -y default-jdk
 
 curl -s https://d2s8p88vqu9w66.cloudfront.net/releases/greengrass-nucleus-latest.zip > greengrass-nucleus-latest.zip && unzip greengrass-nucleus-latest.zip -d GreengrassCore
 
-sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassCore/lib/Greengrass.jar --aws-region eu-central-1 --thing-name GreengrassQuickStartCore-182d87b5203 --thing-group-name GreengrassQuickStartGroup --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
+sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassCore/lib/Greengrass.jar --aws-region eu-central-1 --thing-name GreengrassQuickStartCore-182db0993a8 --thing-group-name GreengrassQuickStartGroup --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
 

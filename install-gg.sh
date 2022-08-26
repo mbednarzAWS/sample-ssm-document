3d76d39e77527d69afcdfed73818015f33a63304
Marcin Bednarz <mbz@amazon.com>
2022-08-26 00:49:08 -0500

    sample ssm doc

---
+++ install-gg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install -y unzip 
+sudo apt install -y default-jdk
+
+curl -s https://d2s8p88vqu9w66.cloudfront.net/releases/greengrass-nucleus-latest.zip > greengrass-nucleus-latest.zip && unzip greengrass-nucleus-latest.zip -d GreengrassCore
+
+sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassCore/lib/Greengrass.jar --aws-region eu-central-1 --thing-name GreengrassQuickStartCore-182d87b5203 --thing-group-name GreengrassQuickStartGroup --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
+
